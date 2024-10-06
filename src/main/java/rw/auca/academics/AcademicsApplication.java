7866f4c0806bae86d5008870ffd2c07486819797
Franki99 <ntaganiradivanni@gmail.com>
2024-10-06 13:27:07 +0300

    tested table population, user model & Repository creation

--- src/main/java/rw/auca/academics/AcademicsApplication.java
+++ src/main/java/rw/auca/academics/AcademicsApplication.java
@@ -1,13 +1,35 @@
 package rw.auca.academics;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import rw.auca.academics.repository.UserRepository;
+import rw.auca.academics.model.User;
+
+
 
 @SpringBootApplication
-public class AcademicsApplication {
+public class AcademicsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AcademicsApplication.class, args);
-	}
+    private final UserRepository userRepository;
 
+    public AcademicsApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("password");
+        userRepository.save(user);
+
+        System.out.println("User saved: " + userRepository.findByEmail("test@example.com").orElse(null));
+    }
 }
+
---
+++ src/main/java/rw/auca/academics/model/User.java
@@ -0,0 +1,15 @@
+package rw.auca.academics.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+}
